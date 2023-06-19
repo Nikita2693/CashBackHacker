05be9a4dda98d981b468d0f1cef0a7fca6e04de0
Nikita2693 <belkinnik93@gmail.com>
2023-06-19 21:37:54 +0300

    Add more Test

--- src/test/java/ru/netology/service/CashBackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackerServiceTest.java
@@ -21,10 +21,37 @@ public class CashBackHackerServiceTest {
 
     }
     @org.junit.Test
-    public void testRemainAmountZero() {
+    public void testRemainAmountOverLimit() {
         CashBackHackerService service = new CashBackHackerService();
         int amount = 1500;
         int expected = 500;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected,actual);
+
+    }
+    @org.junit.Test
+    public void testRemainAmountIsLimit() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected,actual);
+
+    }
+    @org.junit.Test
+    public void testRemainAmountMax() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        Assert.assertEquals(expected,actual);
+
+    }
+    @org.junit.Test
+    public void testRemainAmountZero() {
+        CashBackHackerService service = new CashBackHackerService();
+        int amount = 0;
+        int expected = 1000;
         int actual = service.remain(amount);
         Assert.assertEquals(expected,actual);
 
